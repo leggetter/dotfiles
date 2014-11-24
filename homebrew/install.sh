77c45e542624a0f95ef7b062bf9aa2cf87535ec0
leggetter <phil@leggetter.co.uk>
2014-11-24 13:29:14 +0000

    Updates after getting new Mac

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,13 +7,12 @@
 
 # Check for Homebrew
 if test ! $(which brew)
-then
+  then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
 # Install homebrew packages
 brew install grc coreutils spark
-brew install tree
 
 exit 0
